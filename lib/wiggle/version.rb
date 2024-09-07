6ad6363bdcf03bfb022ba3d3dfe0014ca5353a83
Max Hampton <max.hampton@gmail.com>
2024-09-07 13:33:19 -0600

    initilize project

---
+++ lib/wiggle/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Wiggle
+  VERSION = "0.1.0"
+end
---
+++ spec/wiggle_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Wiggle do
+  it "has a version number" do
+    expect(Wiggle::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
